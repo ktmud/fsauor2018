c92519f281b5831377bd56ac4daee2900bbe21bd
Jesse Yang <hello@yjc.me>
2018-11-15 02:33:24 -0500

    Add ElMo models
    Former-commit-id: 03651cd2e35ec92153e3679b177580fdc4e32043
    Former-commit-id: 3791c04909cc4e3746325958e5b156e2735c5e47 [formerly dbda676b417bd1d9b810ba2ae19292fd0b65353e] [formerly 88ce27a15d0fbe72936b8e73ea8ca62ecb177696] [formerly 74f4f89a7cb53eb631715719f71c9fe2cf3ce40a [formerly e95eecebc9b1bc9a6307731784c74a8aab318340] [formerly 88ce27a15d0fbe72936b8e73ea8ca62ecb177696 [formerly 7811587f3ee9b687f2d0a378d1b2a171d733be47]]]
    Former-commit-id: 74f4f89a7cb53eb631715719f71c9fe2cf3ce40a [formerly e95eecebc9b1bc9a6307731784c74a8aab318340]
    Former-commit-id: 74f4f89a7cb53eb631715719f71c9fe2cf3ce40a
    Former-commit-id: 6246a5dcedf4a1e9621bdc2479daa3d3f0d0aa1d

---
+++ fgclassifier/deep/elmo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ELMo: Deep contextualized word representations
+
+- https://allennlp.org/elmo
+- https://github.com/HIT-SCIR/ELMoForManyLangs
+"""
+from elmoformanylangs import Embedder
+
+def get_embedder():
+    Embedder()
+
