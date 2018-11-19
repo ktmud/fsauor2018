5564fb31af0b1e5b0746083d2233e9901dd4b7e1
Jesse Yang <hello@yjc.me>
2018-11-19 14:39:10 -0500

    Separate feature build and classifying process
    Former-commit-id: 941ae6061a01736246f6121b227ce5b14192a164
    Former-commit-id: b5664c8566c88d44e52e77c5da033fd546a2f88f [formerly 3421466453ed26aa19659c333db26fe0768540c8] [formerly 41995d05cbcb4438f9b4f3e2dcb933c509a661f7] [formerly 6f07379dbab8078bddf11cf8567b2e90137e0681 [formerly a6e2c04219c663ecdc25eab8830f18815703fb90] [formerly 41995d05cbcb4438f9b4f3e2dcb933c509a661f7 [formerly 90537e2ace7dc58be449f0bb4be0b77a5326c73a]]]
    Former-commit-id: 6f07379dbab8078bddf11cf8567b2e90137e0681 [formerly a6e2c04219c663ecdc25eab8830f18815703fb90]
    Former-commit-id: 6f07379dbab8078bddf11cf8567b2e90137e0681
    Former-commit-id: e5e143fa1974466147ba9161bb346628c575ac64

--- fgclassifier/baseline.py
+++ fgclassifier/baseline.py
@@ -51,69 +51,6 @@ class SVD(TruncatedSVD):
         return super().fit_transform(*args, **kwargs)
 
 
-class Baseline(Pipeline):
-    """
-    The Baseline model
-
-    Defaults to Tf-idf + TruncatedSVD, a.k.a, Latent Semantic Analysis
-    Classifier uses Complement Naive Bayes.
-    """
-    # Default feature pipeline
-    FEATURE_PIPELINE = [
-        # from one of our sample, words like 味道 (taste), 好 (good)
-        # are also in the most frequent words, but they are actually relevant
-        # in terms of sentiment.
-        ('tfidf', Tfidf(analyzer='word', ngram_range=(1, 5),
-                        min_df=0.01, max_df=0.99, norm='l2')),
-        ('reduce_dim', SVD(n_components=500))
-    ]
-
-    def __init__(self, classifier=None, steps=None):
-        if steps is None:
-            steps = [(name, sk_clone(estimator))
-                     for name, estimator in self.FEATURE_PIPELINE]
-        if classifier is not None:
-            if callable(classifier):
-                classifier = classifier()
-            classifier_name = classifier.__class__.__name__
-            classifier = MultiOutputClassifier(classifier)
-            steps.append((classifier_name, classifier))
-        super().__init__(steps)
-
-    @property
-    def classifier_name(self):
-        return self.steps[-1][0]
-
-    @property
-    def name(self):
-        return self.__class__.__name__ + '_' + self.classifier_name
-
-    def f1_scores(self, X, y):
-        """Return f1 score on a test dataset"""
-        y_pred = self.predict(X)
-        scores = []
-        logger.info('[Validate]: F1 Scores')
-        for i, label in enumerate(y.columns):
-            score = f1_score(y[label], y_pred[:, i], average='macro')
-            scores.append(score)
-            logger.info('  {: <40s}\t{:.4f}'.format(label, score))
-        return scores
-
-    def score(self, X, y):
-        scores = self.f1_scores(X, y)
-        return np.mean(scores)
-
-    def pred_df(self, df, save_to=None):
-        """Make prediction on a data frame and save output"""
-        # read_data returns a copy of df
-        X, y, df = read_data(df, return_df=True)
-        df['content'] = ''
-        df[y.columns] = self.pred(X)
-        if save_to:
-            df.to_csv(save_to, encoding="utf_8_sig", index=False)
-        return df
-
-
 class SparseToSense():
     """Return content length as features.
     do nothing to the labels"""
@@ -140,6 +77,103 @@ class DummyTransform():
         
     def transform(self, X):
         return np.array(X['content'].str.len())[:, None]
+
+
+# ------- Feature Builder -----------------
+
+class BaselineFeature(Pipeline):
+
+    # Default feature pipeline
+    FEATURE_PIPELINE = [
+        # from one of our sample, words like 味道 (taste), 好 (good)
+        # are also in the most frequent words, but they are actually relevant
+        # in terms of sentiment.
+        ('tfidf', Tfidf(analyzer='word', ngram_range=(1, 5),
+                        min_df=0.01, max_df=0.99, norm='l2')),
+        ('reduce_dim', SVD(n_components=500))
+    ]
+
+    def __init__(self, steps=None):
+        super().__init__(steps or self.FEATURE_PIPELINE)
+
+
+def build_features(df_train, df_test, steps=None):
+    X_train, y_train = read_data(df_train)
+    X_test, y_test = read_data(df_test)
+
+    # make sure steps are named tuples
+    steps_ = []
+    for i, estimator in enumerate(steps):
+        if isinstance(estimator, tuple) or isinstance(estimator, list):
+            steps_.append(estimator)
+        else:
+            steps_.append((f'step{i}', estimator))
+
+    feature = BaselineFeature(steps=steps_)
+    X_train = feature.fit_transform(X_train)
+    X_test = feature.transform(X_test)
+    return X_train, y_train, X_test, y_test
+
+
+# ----------- Classifer Model --------------
+
+class Baseline(Pipeline):
+    """
+    The Baseline model
+
+    Defaults to Tf-idf + TruncatedSVD, a.k.a, Latent Semantic Analysis
+    Classifier uses Complement Naive Bayes.
+    """
+
+    def __init__(self, classifier=None, steps=None):
+        steps = steps or []
+        if classifier is not None:
+            if callable(classifier):
+                classifier = classifier()
+            classifier_name = classifier.__class__.__name__
+            classifier = MultiOutputClassifier(classifier)
+            steps.append((classifier_name, classifier))
+        for i, (name, estimator) in enumerate(steps):
+            # Allowing passining classes to use default parameters
+            if callable(estimator):
+                steps[i] = (name, estimator())
+        if not isinstance(steps[-1][1], MultiOutputClassifier):
+            # This last step must be a MultiOutputClassifier
+            steps[-1] = (steps[-1][0], MultiOutputClassifier(steps[-1][1]))
+        super().__init__(steps)
+
+    @property
+    def classifier_name(self):
+        return self.steps[-1][0]
+
+    @property
+    def name(self):
+        return self.__class__.__name__ + '_' + self.classifier_name
+
+    def f1_scores(self, X, y):
+        """Return f1 score on a test dataset"""
+        y_pred = self.predict(X)
+        scores = []
+        logger.info('[Validate]: F1 Scores')
+        for i, label in enumerate(y.columns):
+            score = f1_score(y[label], y_pred[:, i], average='macro')
+            scores.append(score)
+            logger.info('  {: <40s}\t{:.4f}'.format(label, score))
+        return scores
+
+    def score(self, X, y):
+        scores = self.f1_scores(X, y)
+        return np.mean(scores)
+
+    def predict_df(self, df, save_to=None):
+        """Make prediction on a data frame and save output"""
+        # read_data returns a copy of df
+        X, y, df = read_data(df, return_df=True)
+        df['content'] = ''
+        df[y.columns] = self.pred(X)
+        if save_to:
+            df.to_csv(save_to, encoding="utf_8_sig", index=False)
+        return df
 
 
 class Dummy(Baseline):
--- fgclassifier/classifiers.py
+++ fgclassifier/classifiers.py
@@ -3,13 +3,14 @@ Different Classifier models
 """
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, ComplementNB
 from sklearn.svm import SVC, LinearSVC
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, RidgeClassifierCV
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.dummy import DummyClassifier
+from sklearn.multioutput import MultiOutputClassifier
 
 DummyStratified = DummyClassifier(strategy='stratified')
 DummyMostFrequent = DummyClassifier(strategy='most_frequent')
-ExtraTree = ExtraTreesClassifier(n_estimators=100)
+ExtraTree = ExtraTreesClassifier(n_estimators=50)
